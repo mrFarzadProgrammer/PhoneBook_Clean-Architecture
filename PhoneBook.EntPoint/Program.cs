17e594e9b1508b2d12d98e8560f10643ab1f1e1a
Farzad Mohammadi <farzadmohamadi201@gmail.com>
2023-10-02 16:36:07 +0330

    Implement Design Presentation

--- PhoneBook.EntPoint/Program.cs
+++ PhoneBook.EntPoint/Program.cs
@@ -1,8 +1,6 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using UI.Forms;
 
 namespace PhoneBook.EntPoint
 {
@@ -17,7 +15,7 @@ namespace PhoneBook.EntPoint
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new frmMain());
         }
     }
 }
